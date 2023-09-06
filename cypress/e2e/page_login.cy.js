8b811eb95ac39bc2dcdc4d560eb1e8a4b14c7daa
sariwahyunita <wahyunitas@gmail.com>
2023-09-06 20:16:05 +0700

    add tc login

--- cypress/e2e/page_login.cy.js
+++ cypress/e2e/page_login.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('page login', () => {
+  it('user login', () => {
+    cy.visit('https://katalon-demo-cura.herokuapp.com')
+    cy.contains("CURA Healthcare Service")
+    cy.contains("Make Appointment").click()
+    cy.contains("Login")
+    cy.url().should('include', '/profile.php#login')
+    cy.get('#txt-username').type("John Doe")
+    cy.get('#txt-password').type("ThisIsNotAPassword")
+    cy.get('#btn-login').click()
+    cy.url().should('include', '/#appointment')
   })
 })
